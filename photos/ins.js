392056321c2d0f83bb6f32da4b1c0097fe716d66
cyh42 <cyh42@outlook.com>
2020-02-02 18:58:47 +0800

    Site updated: 2020-02-02 18:58:46

--- photos/ins.js
+++ photos/ins.js
@@ -116,7 +116,7 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://q52lkxy2c.bkt.clouddn.com/' + data.link[i];
+          var minSrc = 'http://q52m4qipk.bkt.clouddn.com/' + data.link[i];
           var src = 'http://q52lkxy2c.bkt.clouddn.com/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
